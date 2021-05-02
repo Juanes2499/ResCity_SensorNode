c17b57d632f8b01adcd65480d0d073b59e6c67c4
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-05-02 05:10:58 -0500

    Se avanza en el desarrollo de la comunicación seria para enviar los datos por medio WIFI

--- NodeMCU/NodeMCU.ino
+++ NodeMCU/NodeMCU.ino
@@ -1,35 +1,26 @@
-
-
-//#include <WiFiEsp.h>
-//#include <PubSubClient.h>
-//#include <SoftwareSerial.h>
-
-//const int ledPIN = 2;
-// 
-//void setup() {
-//  Serial.begin(9600);    //iniciar puerto serie
-//  pinMode(ledPIN , OUTPUT);  //definir pin como salida
-//}
-// 
-//void loop(){
-//  digitalWrite(ledPIN , HIGH);   // poner el Pin en HIGH
-//  delay(1000);                   // esperar un segundo
-//  digitalWrite(ledPIN , LOW);    // poner el Pin en LOW
-//  delay(1000);                   // esperar un segundo
-//}
-
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
+
 
 //Credenciales de la red WIFI
 const char* ssid = "402C";
 const char* password = "nichoy99082411425";
 
 //Host del servidor:
-String host = "http://192.168.0.17:3010/api";
+String host = "http://192.168.0.17:3010/api/publishDatosNodo";
+
+//Datos del nodo sensor
+String id_nodo_sensor = "901ee534-4ca1-4932-a4d8-ef5c4e64dba1";
+const char* token_dispositivo = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF9kaXNwb3NpdGl2byI6IjkwMWVlNTM0LTRjYTEtNDkzMi1hNGQ4LWVmNWM0ZTY0ZGJhMSIsIm1hcmNhIjoiVGVzdCIsInJlZmVyZW5jaWEiOiJUZXN0IHJlZiIsImxhdGl0dWQiOiIxMTExLjk5OSIsImxvbmdpdHVkIjoiLTEuODk4NyIsIm5vbWJyZV9taWNyb3NlcnZpY2lvIjoiU0VOU09SRVNfTlMiLCJlbWFpbF9yZXNwb25zYWJsZSI6Imp1YW4ubmljaG95QHVhby5lZHUuY28iLCJpYXQiOjE2MTg5NTUwNTl9.NprlCdYG18Of5pUVWOKpBnXtPu5s3kSZZ5Bb7g2nVuo";
+const char* token_autenticacion = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRF9ESVNQT1NJVElWTyI6IjkwMWVlNTM0LTRjYTEtNDkzMi1hNGQ4LWVmNWM0ZTY0ZGJhMSIsIlRPS0VOIjoiZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBaRjlrYVhOd2IzTnBkR2wyYnlJNklqa3dNV1ZsTlRNMExUUmpZVEV0TkRrek1pMWhOR1E0TFdWbU5XTTBaVFkwWkdKaE1TSXNJbTFoY21OaElqb2lWR1Z6ZENJc0luSmxabVZ5Wlc1amFXRWlPaUpVWlhOMElISmxaaUlzSW14aGRHbDBkV1FpT2lJeE1URXhMams1T1NJc0lteHZibWRwZEhWa0lqb2lMVEV1T0RrNE55SXNJbTV2YldKeVpWOXRhV055YjNObGNuWnBZMmx2SWpvaVUwVk9VMDlTUlZOZlRsTWlMQ0psYldGcGJGOXlaWE53YjI1ellXSnNaU0k2SW1wMVlXNHVibWxqYUc5NVFIVmhieTVsWkhVdVkyOGlMQ0pwWVhRaU9qRTJNVGM1TXpRNU5ERXNJbVY0Y0NJNk1UWXhOemswTkRrME1IMC5wanBJOXoxUURXZmNwUFBYRHdCYk91enpEcFFhTGk1QlBzd1k5Q0pYR2tJIiwiTUFSQ0EiOiJUZXN0IiwiUkVGRVJFTkNJQSI6IlRlc3QgcmVmIiwiTk9NQlJFX01JQ1JPU0VSVklDSU8iOiJTRU5TT1JFU19OUyIsIkVNQUlMX1JFU1BPTlNBQkxFIjoianVhbi5uaWNob3lAdWFvLmVkdS5jbyIsIkRJU1BPU0lUSVZPX0FDVElWTyI6MSwiaWF0IjoxNjE3OTM3NDA2LCJleHAiOjE2NDk0NzM0MDZ9.8wiuN6xfS_Fc1wPw0UG7HESs7lWHr3QOFvGF5d2cpn8";
 
 const int greenPIN = 2;
 const int redPIN = 15;
+const int bluePIN = 0;
+
+bool messageComplete = false;
+String messageEsclavo = "";
 
 void setup() {
  
@@ -39,6 +30,9 @@ void setup() {
   
   pinMode(greenPIN , OUTPUT);  //definir pin como salida
   pinMode(redPIN , OUTPUT);  //definir pin como salida
+  pinMode(bluePIN , OUTPUT);  //definir pin como salida
+
+  messageEsclavo.reserve(200);
  
   while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(redPIN , HIGH);   // poner el Pin en HIGH
@@ -57,42 +51,74 @@ void loop() {
   HTTPClient http;
   WiFiClient client;
 
-   if (Serial.available()) {
-    Serial.write(Serial.read());
-  }
-
-  delay(2000);
+  StaticJsonDocument<256> doc;
+  JsonObject json = doc.to<JsonObject>();
 
   if(WiFi.status() == WL_CONNECTED){
-    digitalWrite(redPIN , LOW);   // poner el Pin en HIGH
-    delay(500); 
-    digitalWrite(greenPIN , HIGH);   // poner el Pin en HIGH
-    delay(500);                   // esperar un segundo
+
+    //Control de indicacores conexión WIFI
+    digitalWrite(redPIN , LOW);   
+    delay(100); 
+    digitalWrite(greenPIN , HIGH);   
+    delay(100);     
+
+    http.begin(host);
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("Authorization", token_autenticacion);
+
+    json["id_nodo_sensor"] = id_nodo_sensor;
+    json["token"] = token_dispositivo;
+    
+    JsonObject variables = json.createNestedObject("variables");
+
+    if(messageComplete == true){
+        DeserializationError error = deserializeJson(doc,messageEsclavo);
+        if(error) {
+          Serial.print(F("Fallo en deserialziación JSON: "));
+          Serial.println(error.c_str());
+          return;
+        }
+
+        variables["nivel_agua"] = doc["nivel_agua"];
+
+        String output;
+        serializeJsonPretty(json, output);
+        
+        int httpResponseCode = http.POST(output);
+    
+        if(httpResponseCode ==  HTTP_CODE_OK){
+          digitalWrite(bluePIN , HIGH);
+          delay(100);    
+          http.end();
+        }else{
+          String payload = http.getString(); 
+          Serial.println(payload);
+        }
+    }
+    
+    delay(1000);
+    
   }else{
     digitalWrite(greenPIN , LOW);   // poner el Pin en HIGH
-    delay(500);                   // esperar un segundo
+    delay(100);                   // esperar un segundo
     digitalWrite(redPIN , HIGH);   // poner el Pin en HIGH
-    delay(500); 
+    delay(100); 
   }
-      
-  if (http.begin(client, host)) //Iniciar conexión
-  {
-    Serial.print("[HTTP] GET...\n");
-    int httpCode = http.GET();  // Realizar petición
-    if (httpCode > 0) {
-       Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-       if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          String payload = http.getString();   // Obtener respuesta
-          Serial.println(payload);   // Mostrar respuesta por serial
-       }
+
+  digitalWrite(bluePIN , LOW);   
+  messageComplete = false;
+  messageEsclavo = "";
+  delay(3000);
+}
+
+void serialEvent(){
+  Serial.println("Hola1");
+  while(Serial.available() > 0){
+    Serial.println("Hola2");
+    char inChar = (char)Serial.read();
+    messageEsclavo += inChar;
+    if(inChar == '}'){
+      messageComplete = true;
     }
-    else {
-       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
-    http.end();
   }
-  else {
-    Serial.printf("[HTTP} Unable to connect\n");
-  }
-  delay(2000);
 }
--- pruebaArduino/pruebaArduino.ino
+++ pruebaArduino/pruebaArduino.ino
@@ -8,30 +8,37 @@ int distancia;  //crea la variable "distancia"
 float tiempo;  //crea la variable tiempo (como float)
 String str;
 
-SoftwareSerial espSerial(5, 6);
+String message = "";
+bool messageReady = false;
+
+//SoftwareSerial espSerial(17,16); //RX TX
  
 void setup()
 {
   Serial.begin(115200);  //inicializa el puerto serie
-  espSerial.begin(115200);
+  //espSerial.begin(4800);
   pinMode(pulso, OUTPUT); //Declaramos el pin 9 como salida (pulso ultrasonido)
   pinMode(rebote, INPUT); //Declaramos el pin 8 como entrada (recepción del pulso)
 }
  
 void loop(){
+  
   digitalWrite(pulso,LOW); //Por cuestión de estabilización del sensor
   delayMicroseconds(5);
   digitalWrite(pulso, HIGH); //envío del pulso ultrasónico
   delayMicroseconds(10);
   tiempo = pulseIn(rebote, HIGH);  //funcion para medir el tiempo y guardarla en la variable "tiempo"
   distancia = 0.01715*tiempo; //fórmula para calcular la distancia
-   
-  /*Monitorización en centímetros por el monitor serial*/
-  Serial.print("Distancia: ");
-  Serial.print(distancia);
-  Serial.println(" cm");
-  str =String("Distancia: ")+String(distancia);
-  espSerial.println(str);
-  delay(1000);
+
+  //Enviar datos al ESP32
+  StaticJsonDocument<1000> doc;
+  JsonObject json = doc.to<JsonObject>();
+
+  json["nivel_agua"] = String(distancia);
+
+  String output;
+  serializeJson(json, Serial);
+  
+  delay(5000);
 }
  
